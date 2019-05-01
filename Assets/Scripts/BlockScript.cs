99ad8ad76b7bfe6fd4a30e33bf89945b5619d1d1
Claudio Domene <claudio79jr@gmail.com>
2019-05-01 16:42:57 -0300

    inicializando

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour {
+
+    public Sprite obj;
+    public Sprite flag;
+    
+    private SpriteRenderer sr;
+    private Sprite blank;
+
+    private bool mouseHere;
+
+    private void Start() {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+
+        blank = sr.sprite;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) && mouseHere) LeftClick();
+
+        if (Input.GetMouseButtonDown(1) && mouseHere) RightClick();
+    }
+
+    private void OnMouseEnter() { mouseHere = true; }
+
+    private void OnMouseExit() { mouseHere = false; }
+    
+    private void RightClick() {
+        if (sr.sprite == blank) {
+            sr.sprite = flag;
+
+            transform.parent.SendMessage("SetFlag", transform.position);
+        }
+
+        else {
+            sr.sprite = blank;
+
+            transform.parent.SendMessage("RemoveFlag", transform.position);
+        }
+    }
+
+    private void LeftClick() { sr.sprite = obj; }
+
+    public void SetOcultSprite(Sprite sprite) { obj = sprite; }
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour {
+
+    public int numBombs;
+    public int gridSize;
+    public Sprite[] spritesObj;
+    public GameObject blockPrefab;
+
+    private int[,] grid;
+
+    void Start() {
+        if(numBombs >= gridSize * gridSize)
+            Debug.LogError("grid muito pequeno para quantidade de bombas");
+
+        CreateGrid();
+
+        CreateMap();
+    }
+
+    private void CreateGrid() {
+        grid = new int[gridSize, gridSize];
+
+        int x, y, z = 0;
+
+        while (z < numBombs) {
+            x = Random.Range(0, gridSize);
+
+            y = Random.Range(0, gridSize);
+
+            if (grid[x, y] != 0) continue;
+
+            grid[x, y] = 9;
+
+            z++;
+        }
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                int n = 0, k = gridSize;
+
+                if (grid[i, j] == 9) continue;
+
+                if (i + 1 < k && grid[i + 1, j] == 9) n++;
+                if (j + 1 < k && grid[i, j + 1] == 9) n++;
+                if (i - 1 >= 0 && grid[i - 1, j] == 9) n++;
+                if (j - 1 >= 0 && grid[i, j - 1] == 9) n++;
+                if (i + 1 < k && j + 1 < k && grid[i + 1, j + 1] == 9) n++;
+                if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1, j - 1] == 9) n++;
+                if (i - 1 >= 0 && j + 1 < k && grid[i - 1, j + 1] == 9) n++;
+                if (i + 1 < k && j - 1 >= 0 && grid[i + 1, j - 1] == 9) n++;
+
+                grid[i, j] = n;
+            }
+        }
+    }
+
+    private void CreateMap() {
+        for (int i = 0; i < gridSize; i++)
+            for (int j = 0; j < gridSize; j++) {
+                GameObject tile = Instantiate(blockPrefab, new Vector3(i, j, 0f), Quaternion.identity);
+
+                tile.transform.parent = gameObject.transform;
+
+                tile.SendMessage("SetOcultSprite", spritesObj[grid[i, j]]);
+            }
+    }
+
+    public void RemoveFlag() {
+
+    }
+
+    public void SetFlag() {
+
+    }
+}
